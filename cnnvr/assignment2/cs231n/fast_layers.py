d022459be05b57fddf7416bf3445ee890e7c6c67
nr <shibuyanorailgun@foxmail.com>
2016-03-01 19:45:33 +0800

    cnnvr new assignment 2

--- cnnvr/assignment2/cs231n/fast_layers.py
+++ cnnvr/assignment2/cs231n/fast_layers.py
@@ -1,6 +1,7 @@
 import numpy as np
 try:
   from cs231n.im2col_cython import col2im_cython, im2col_cython
+  from cs231n.im2col_cython import col2im_6d_cython
 except ImportError:
   print 'run the following from the cs231n directory and try again:'
   print 'python setup.py build_ext --inplace'
@@ -9,7 +10,7 @@ except ImportError:
 from cs231n.im2col import *
 
 
-def conv_forward_fast(x, w, b, conv_param):
+def conv_forward_im2col(x, w, b, conv_param):
   """
   A fast implementation of the forward pass for a convolutional layer
   based on im2col and col2im.
@@ -38,7 +39,71 @@ def conv_forward_fast(x, w, b, conv_param):
   return out, cache
 
 
-def conv_backward_fast(dout, cache):
+def conv_forward_strides(x, w, b, conv_param):
+  N, C, H, W = x.shape
+  F, _, HH, WW = w.shape
+  stride, pad = conv_param['stride'], conv_param['pad']
+
+  # Check dimensions
+  assert (W + 2 * pad - WW) % stride == 0, 'width does not work'
+  assert (H + 2 * pad - HH) % stride == 0, 'height does not work'
+
+  # Pad the input
+  p = pad
+  x_padded = np.pad(x, ((0, 0), (0, 0), (p, p), (p, p)), mode='constant')
+  
+  # Figure out output dimensions
+  H += 2 * pad
+  W += 2 * pad
+  out_h = (H - HH) / stride + 1
+  out_w = (W - WW) / stride + 1
+
+  # Perform an im2col operation by picking clever strides
+  shape = (C, HH, WW, N, out_h, out_w)
+  strides = (H * W, W, 1, C * H * W, stride * W, stride)
+  strides = x.itemsize * np.array(strides)
+  x_stride = np.lib.stride_tricks.as_strided(x_padded,
+                shape=shape, strides=strides)
+  x_cols = np.ascontiguousarray(x_stride)
+  x_cols.shape = (C * HH * WW, N * out_h * out_w)
+
+  # Now all our convolutions are a big matrix multiply
+  res = w.reshape(F, -1).dot(x_cols) + b.reshape(-1, 1)
+
+  # Reshape the output
+  res.shape = (F, N, out_h, out_w)
+  out = res.transpose(1, 0, 2, 3)
+
+  # Be nice and return a contiguous array
+  # The old version of conv_forward_fast doesn't do this, so for a fair
+  # comparison we won't either
+  out = np.ascontiguousarray(out)
+
+  cache = (x, w, b, conv_param, x_cols)
+  return out, cache
+  
+
+def conv_backward_strides(dout, cache):
+  x, w, b, conv_param, x_cols = cache
+  stride, pad = conv_param['stride'], conv_param['pad']
+
+  N, C, H, W = x.shape
+  F, _, HH, WW = w.shape
+  _, _, out_h, out_w = dout.shape
+
+  db = np.sum(dout, axis=(0, 2, 3))
+
+  dout_reshaped = dout.transpose(1, 0, 2, 3).reshape(F, -1)
+  dw = dout_reshaped.dot(x_cols.T).reshape(w.shape)
+
+  dx_cols = w.reshape(F, -1).T.dot(dout_reshaped)
+  dx_cols.shape = (C, HH, WW, N, out_h, out_w)
+  dx = col2im_6d_cython(dx_cols, N, C, H, W, HH, WW, pad, stride)
+
+  return dx, dw, db
+
+
+def conv_backward_im2col(dout, cache):
   """
   A fast implementation of the backward pass for a convolutional layer
   based on im2col and col2im.
@@ -58,6 +123,10 @@ def conv_backward_fast(dout, cache):
                      filter_height, filter_width, pad, stride)
 
   return dx, dw, db
+
+
+conv_forward_fast = conv_forward_strides
+conv_backward_fast = conv_backward_strides
 
 
 def max_pool_forward_fast(x, pool_param):
--- cnnvr/assignment2/cs231n/im2col_cython.pyx
+++ cnnvr/assignment2/cs231n/im2col_cython.pyx
@@ -2,8 +2,12 @@ import numpy as np
 cimport numpy as np
 cimport cython
 
-DTYPE = np.float64
-ctypedef np.float64_t DTYPE_t
+# DTYPE = np.float64
+# ctypedef np.float64_t DTYPE_t
+
+ctypedef fused DTYPE_t:
+    np.float32_t
+    np.float64_t
 
 def im2col_cython(np.ndarray[DTYPE_t, ndim=4] x, int field_height,
                   int field_width, int padding, int stride):
@@ -20,26 +24,14 @@ def im2col_cython(np.ndarray[DTYPE_t, ndim=4] x, int field_height,
             ((0, 0), (0, 0), (p, p), (p, p)), mode='constant')
 
     cdef np.ndarray[DTYPE_t, ndim=2] cols = np.zeros(
-            (C * field_height * field_width, N * HH * WW))
+            (C * field_height * field_width, N * HH * WW),
+            dtype=x.dtype)
 
     # Moving the inner loop to a C function with no bounds checking works, but does
     # not seem to help performance in any measurable way.
 
     im2col_cython_inner(cols, x_padded, N, C, H, W, HH, WW,
                         field_height, field_width, padding, stride)
-    # cdef int yy, xx, y0, y1, x0, x1, i
-    # cdef int ii, jj, c, row, col
-
-    # for c in range(C):
-    #     for ii in range(field_height):
-    #         for jj in range(field_width):
-    #             row = c * field_width * field_height + ii * field_height + jj
-    #             for yy in range(HH):
-    #                 for xx in range(WW):
-    #                     for i in range(N):
-    #                         col = yy * WW * N + xx * N + i
-    #                         cols[row, col] = x_padded[i, c, stride * yy + ii, stride * xx + jj]
-    
     return cols
 
 
@@ -51,23 +43,24 @@ cdef int im2col_cython_inner(np.ndarray[DTYPE_t, ndim=2] cols,
     cdef int c, ii, jj, row, yy, xx, i, col
 
     for c in range(C):
-        for ii in range(field_height):
-            for jj in range(field_width):
-                row = c * field_width * field_height + ii * field_height + jj
-                for yy in range(HH):
-                    for xx in range(WW):
+        for yy in range(HH):
+            for xx in range(WW):
+                for ii in range(field_height):
+                    for jj in range(field_width):
+                        row = c * field_width * field_height + ii * field_height + jj
                         for i in range(N):
                             col = yy * WW * N + xx * N + i
                             cols[row, col] = x_padded[i, c, stride * yy + ii, stride * xx + jj]
 
 
+
 def col2im_cython(np.ndarray[DTYPE_t, ndim=2] cols, int N, int C, int H, int W,
                   int field_height, int field_width, int padding, int stride):
-    cdef np.ndarray x = np.empty((N, C, H, W), dtype=DTYPE)
+    cdef np.ndarray x = np.empty((N, C, H, W), dtype=cols.dtype)
     cdef int HH = (H + 2 * padding - field_height) / stride + 1
     cdef int WW = (W + 2 * padding - field_width) / stride + 1
     cdef np.ndarray[DTYPE_t, ndim=4] x_padded = np.zeros((N, C, H + 2 * padding, W + 2 * padding),
-                                        dtype=DTYPE)
+                                        dtype=cols.dtype)
 
     # Moving the inner loop to a C-function with no bounds checking improves
     # performance quite a bit for col2im.
@@ -94,3 +87,35 @@ cdef int col2im_cython_inner(np.ndarray[DTYPE_t, ndim=2] cols,
                         for i in range(N):
                             col = yy * WW * N + xx * N + i
                             x_padded[i, c, stride * yy + ii, stride * xx + jj] += cols[row, col]
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef col2im_6d_cython_inner(np.ndarray[DTYPE_t, ndim=6] cols,
+                            np.ndarray[DTYPE_t, ndim=4] x_padded,
+                            int N, int C, int H, int W, int HH, int WW,
+                            int out_h, int out_w, int pad, int stride):
+
+    cdef int c, hh, ww, n, h, w
+    for n in range(N):
+        for c in range(C):
+            for hh in range(HH):
+                for ww in range(WW):
+                    for h in range(out_h):
+                        for w in range(out_w):
+                            x_padded[n, c, stride * h + hh, stride * w + ww] += cols[c, hh, ww, n, h, w]
+    
+
+def col2im_6d_cython(np.ndarray[DTYPE_t, ndim=6] cols, int N, int C, int H, int W,
+        int HH, int WW, int pad, int stride):
+    cdef np.ndarray x = np.empty((N, C, H, W), dtype=cols.dtype)
+    cdef int out_h = (H + 2 * pad - HH) / stride + 1
+    cdef int out_w = (W + 2 * pad - WW) / stride + 1
+    cdef np.ndarray[DTYPE_t, ndim=4] x_padded = np.zeros((N, C, H + 2 * pad, W + 2 * pad),
+                                                  dtype=cols.dtype)
+
+    col2im_6d_cython_inner(cols, x_padded, N, C, H, W, HH, WW, out_h, out_w, pad, stride)
+
+    if pad > 0:
+        return x_padded[:, :, pad:-pad, pad:-pad]
+    return x_padded 
