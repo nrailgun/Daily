5e430fcd04adb209158d0a68273f2c762a770d31
Junyu Wu <shibuyanorailgun@foxmail.com>
2016-12-29 17:36:43 +0800

    Update 75.cpp

--- leetcode/75.cpp
+++ leetcode/75.cpp
@@ -6,6 +6,7 @@ public:
         
         int i, next = 0, j = n - 1;
         for (i = 0; i <= j; i++) {
+            // 注意：先比较 0 还是 2 有巨大的区别！算法这种地方还是比较 tricky 的。
             while (i < j && a[i] == 2) {
                 swap(a[i], a[j--]);
             }
