13919976db868772eee5f9129b44f674cf887684
Junyu Wu <shibuyanorailgun@foxmail.com>
2016-12-31 17:11:24 +0800

    Update 119.cpp

--- leetcode/119.cpp
+++ leetcode/119.cpp
@@ -18,3 +18,50 @@ public:
     }
 };
 // 拓宽思维，习惯上我们处理数组从前往后，实际上从后往前有许多好处，比如 pascal triangle 中可以避免覆盖。
+class Solution {
+public:
+    vector<int> getRow(int k) {
+        vector<int> a(k+1, 0);
+        a[0] = 1;
+        
+        for (int i = 1; i <= k; i++) {
+            for (int j = i; j > 0; j--) {
+                a[j] += a[j-1];
+            }
+        }
+        return a;
+    }
+};
+// 利用 `triangle[n][m] = $C^m_n$` 的性质，但是计算溢出。
+class Solution {
+public:
+    vector<int> getRow(int n) {
+        vector<int> a(n+1, 0);
+        a[0] = 1;
+        
+        int nume = 1, domi = 1;
+        for (int i = 1; i <= n; i++) {
+            nume *= (n - i + 1);
+            domi *= i;
+            a[i] = nume / domi;
+        }
+        return a;
+    }
+};
+// 正解是利用 long long int。
+class Solution {
+public:
+    vector<int> getRow(int n) {
+        vector<int> a(n+1, 0);
+        a[0] = 1;
+        
+        int i;
+        for (i = 1; i <= n / 2; i++) {
+            a[i] = (long long int) a[i - 1] * (n - i + 1) / i;
+        }
+        for (; i <= n; i++) {
+            a[i] = a[n - i];
+        }
+        return a;
+    }
+};
