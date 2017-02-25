7f61f32864438e8293b01341e91b3ffbc99cbc95
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-02-25 21:30:35 +0800

    Update 346.cpp

--- leetcode/346.cpp
+++ leetcode/346.cpp
@@ -1,33 +1,26 @@
 class MovingAverage {
 public:
-    list<double> l;
-    int wsize;
-    double mean;
-
+    queue<double> q;
+    int k;
+    double av;
+    
     /** Initialize your data structure here. */
-    MovingAverage(int size)
-    : wsize(size), mean(0) {
+    MovingAverage(int size) {
+        k = size;
+        av = 0;
     }
     
     double next(int val) {
-        if (l.size() < wsize) {
-            l.push_back(val);
-            mean = accumulate(l.begin(), l.end(), 0);
-            mean = mean / l.size();
-            return mean;
+        int n = q.size();
+        if (n < k) { // 此处只需求和，可避免 n < k 时的 */ 运算
+            av += val; // av = (av * n + val) / (n + 1);
         } else {
-            double hv = l.front();
-            l.erase(l.begin());
-            l.push_back(val);
-            mean -= hv / wsize;
-            mean += (double) val / wsize;
-            return mean;
+            av += val - q.front();
         }
+        
+        if (q.size() == k)
+            q.pop();
+        q.push(val);
+        return av / q.size();
     }
 };
-
-/**
- * Your MovingAverage object will be instantiated and called as such:
- * MovingAverage obj = new MovingAverage(size);
- * double param_1 = obj.next(val);
- */
