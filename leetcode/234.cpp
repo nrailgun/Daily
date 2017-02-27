f19bcf93af4cede36718e0bb64679117c2935457
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-02-27 16:41:30 +0800

    Update 234.cpp

--- leetcode/234.cpp
+++ leetcode/234.cpp
@@ -17,4 +17,38 @@ public:
     }
 };
 
-// O(1) 空间。
+// O(1) 空间。找到中点，翻转，顺序比较。我真是太机智了。
+// 小心置空指针，免得 leetcode release 出错。
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next)
+            return true;
+        
+        ListNode *mid = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            mid = mid->next;
+            fast = fast->next->next;
+        }
+
+        ListNode *last = mid, *n = mid->next;
+        while (n) {
+            ListNode *tmp = n;
+            n = n->next;
+            tmp->next = last;
+            last = tmp;
+        }
+        mid->next->next = nullptr; // 无关算法，但是不置空会导致 leetcode 释放出错
+        mid->next = nullptr;
+        
+        ListNode *n1 = head, *n2 = last;
+        while (n1 && n2) {
+            if (n1->val != n2->val) {
+                return false;
+            }
+            n1 = n1->next;
+            n2 = n2->next;
+        }
+        return true;
+    }
+};
