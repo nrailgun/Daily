d88c1fd85ab2217fcf83d046882e0a3526fb0d48
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-03-03 16:54:31 +0800

    Update 300.cpp

--- leetcode/300.cpp
+++ leetcode/300.cpp
@@ -1,4 +1,5 @@
 // 客官您好，您要的 O(n^2) 解。
+// 虽然 DP 思想更加复杂一些，但是 DP 解法不一定是最好的解法。有些富有灵性的解法可以超过 DP。
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
@@ -19,4 +20,22 @@ public:
     }
 };
 
-// O(n logn)
+// O(n logn)。
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if (!n)
+            return 0;
+        vector<int> res;
+        
+        for (int i = 0; i < n; i++) {
+            auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+            if (it == res.end())
+                res.push_back(nums[i]);
+            else
+                *it = nums[i];
+        }
+        return res.size();
+    }
+};
