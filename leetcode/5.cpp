279a041583bb8fa4e09845a96341dc10d878e4eb
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-03-09 22:41:32 +0800

    Update 5.cpp

--- leetcode/5.cpp
+++ leetcode/5.cpp
@@ -1,68 +1,69 @@
 // 内存爆了。。。
 class Solution {
 public:
-    pair<int, int> palindrome(const string &s, int lb, int ub, bool &is_palindrome,
-        vector<vector<int>> &palindrome_cache, vector<vector<pair<int, int>>> &pair_cache)
-    {
-        if (palindrome_cache[lb][ub] >= 0) {
-            is_palindrome = palindrome_cache[lb][ub];
-            return pair_cache[lb][ub];
+    const char *s;
+
+    pair<int, int> solve(const char *s, int idxi, int idxj, vector<vector<pair<int, int>>> &dp) {
+        if (idxj - idxi <= 0)
+            return make_pair(idxi, 1 + idxj - idxi);
+        
+        if (dp[idxi][idxj].second != -1)
+            return dp[idxi][idxj];
+
+        pair<int, int> l1(idxi, 0);
+        if (s[idxi] == s[idxj]) {
+            l1 = solve(s, idxi + 1, idxj - 1, dp);
+            if (l1.second == idxj - idxi - 1) {
+                return dp[idxi][idxj] = (l1 = make_pair(idxi, l1.second + 2));
+            }
         }
         
-        if (lb + 1 == ub) {
-            is_palindrome = s[lb] == s[ub];
-            if (is_palindrome) {
-                palindrome_cache[lb][ub] = 1;
-                pair_cache[lb][ub] = make_pair(lb, ub);
-            }
-            else {
-                palindrome_cache[lb][ub] = 0;
-                pair_cache[lb][ub] = make_pair(lb, lb);
-            }
-            return pair_cache[lb][ub];
-        }
-        else { // if (s[lb] == s[ub]) {
-            // 注意 s[lb] == s[ub] 的情况，本题求 substring(subarray) 而不是 subsequence，
-            // 所以不一定就是最长。一定小心 subarray 和 subsequence 的区别。
-            
-            pair<int, int> rp;
-            pair<int, int> lub1 = palindrome(s, lb+1, ub, is_palindrome, palindrome_cache, pair_cache);
-            pair<int, int> lub2 = palindrome(s, lb, ub-1, is_palindrome, palindrome_cache, pair_cache);
-            if (lub1.second - lub1.first > lub2.second - lub2.first)
-                rp = lub1;
-            else
-                rp = lub2;
-            
-            if (s[lb] == s[ub]) {
-                pair<int, int> lub = palindrome(s, lb+1, ub-1, is_palindrome, palindrome_cache, pair_cache);
-                if (is_palindrome)
-                    lub = make_pair(lb, ub);
-                if (rp.second - rp.first < lub.second - lub.first)
-                    rp = lub;
-            } else {
-                is_palindrome = false;
-            }
-            
-            if (is_palindrome) {
-                palindrome_cache[lb][ub] = 1;
-            } else {
-                palindrome_cache[lb][ub] = 0;
-            }
-            return pair_cache[lb][ub] = rp;
-        }
+        auto l2 = solve(s, idxi, idxj - 1, dp);
+        auto l3 = solve(s, idxi + 1, idxj, dp);
+        
+        if (l1.second > l2.second && l1.second > l3.second)
+            return dp[idxi][idxj] = l1;
+        else if (l2.second > l3.second)
+            return dp[idxi][idxj] = l2;
+        else
+            return dp[idxi][idxj] = l3;
     }
 
     string longestPalindrome(string s) {
         int n = s.size();
-        vector<vector<int>> palindrome_cache(n, vector<int>(n, -1));
-        vector<vector<pair<int, int>>> pair_cache(n, vector<pair<int, int>>(n));
-        for (int i = 0; i < n; i++) {
-            palindrome_cache[i][i] = 1;
-            pair_cache[i][i] = make_pair(i, i);
-        }
+        vector<vector<pair<int, int>>> dp(n, vector<pair<int, int>>( n, make_pair(-1, -1) ));
         
-        bool isp;
-        pair<int, int> lub = palindrome(s, 0, n-1, isp, palindrome_cache, pair_cache);
-        return s.substr(lub.first, lub.second - lub.first + 1);
+        auto pr = solve(s.data(), 0, s.size()-1, dp);
+        return s.substr(pr.first, pr.second);
     }
 };
+
+// 虽然还是动归，但是优化了内存消耗。不需要每次都记录 (i, j) 内的开始结束点，只需要记录是否回文，
+// 最后再检查一遍。
+class Solution {
+public:
+
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, true));
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = (s[i] == s[j]) && dp[i+1][j-1];
+            }
+        }
+        
+        int maxl = 0, maxi;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (dp[i][j] && j - i + 1 > maxl) {
+                    maxl = j - i + 1;
+                    maxi = i;
+                }
+            }
+        }
+        return s.substr(maxi, maxl);
+    }
+};
+
+// 另外一种方法是，i 从 0 到 n，检查 i 为中点最大能建立多大的 palindrome。理论上 O(n^2) 复杂度，然而实际运行快很多。
