596dd9ef5671d0fa5c70d8ba6c3e8df03a67cae6
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-03-24 13:06:59 +0800

    Create 232.cpp

---
+++ leetcode/232.cpp
@@ -0,0 +1,43 @@
+class MyQueue {
+public:
+    stack<int> s1, s2;
+    
+    /** Initialize your data structure here. */
+    MyQueue() {
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        
+        int rv = s2.top();
+        s2.pop();
+        return rv;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() const {
+        return s1.empty() && s2.empty();
+    }
+};
