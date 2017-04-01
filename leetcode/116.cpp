bbb1420b9bfce94f5506467397a1e7189aeca7f0
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-04-01 16:44:02 +0800

    Update 116.cpp

--- leetcode/116.cpp
+++ leetcode/116.cpp
@@ -5,19 +5,17 @@ public:
       return;
     root->next = NULL;
 
-    TreeLinkNode *head = root, *cur;
-    for (cur = head; cur != NULL; ) {
-      if (!cur->left)
-        break;
-      cur->left->next = cur->right;
-      if (cur->next) {
-        cur->right->next = cur->next->left;
-        cur = cur->next;
+    TreeLinkNode *head = root;
+    while (head->left) {
+      TreeLinkNode *cur = head;
+      for ( ; cur != NULL; cur = cur->next) {
+        cur->left->next = cur->right;
+        if (cur->next)
+          cur->right->next = cur->next->left;
+        else
+          cur->right->next = NULL;
       }
-      else {
-        cur->right->next = NULL;
-        cur = (head = head->left);
-      }
+      head = head->left;
     }
   }
 };
