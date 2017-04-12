9f1c92b811c2c3270704d8301d2dee0c15b2dc08
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-04-12 17:34:47 +0800

    Update NetEase2017.cpp

--- nowcoder/NetEase2017.cpp
+++ nowcoder/NetEase2017.cpp
@@ -64,3 +64,63 @@ int main(int argc, char* argv[])
 #endif
 	return 0;
 }
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstddef>
+#include <cassert>
+#include <cstring>
+
+#include <set>
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <cfloat>
+
+using namespace std;
+
+
+template <typename Pred>
+int bubble_sort(string g, const Pred &pred) {
+	int n = g.size();
+	int nswap = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; i + j < n - 1; j++) {
+			if (pred(g[j], g[j + 1])) {
+				nswap++;
+				swap(g[j], g[j + 1]);
+			}
+		}
+	}
+	return nswap;
+}
+
+int main(int argc, char* argv[])
+{
+#ifdef TEST
+	freopen("Text.txt", "r", stdin);
+#endif
+
+	string group;
+	cin >> group;
+
+	int v1 = bubble_sort(group, less<char>());
+	int v2 = bubble_sort(group, greater<char>());
+	cout << min(v1, v2) << endl;
+
+#ifdef TEST
+	freopen("CON", "r", stdin);
+	system("pause");
+#endif
+	return 0;
+}
