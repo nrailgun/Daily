2722ae3c692edacd9c3b49071c8106665ed21d85
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-05-03 22:46:04 +0800

    Update 339.cpp

--- leetcode/339.cpp
+++ leetcode/339.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
-
-using namespace std;
-
-int main(int argc, char *argv[])
-{
-  cout << "hello, world" << endl;
-  
-  return 0;
-}
+class Solution {
+public:
+    int depthSum(vector<NestedInteger>& nl, int depth = 1) {
+        int s = 0;
+        for (int i = 0; i < nl.size(); i++) {
+            if (nl[i].isInteger()) {
+                s += depth * nl[i].getInteger();
+            } else {
+                s += depthSum(nl[i].getList(), depth + 1);
+            }
+        }
+        return s;
+    }
+};
