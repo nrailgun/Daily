7649e9f8a5e0a0fa8d016b28ac9b2994cc0a0719
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-05-05 22:40:19 +0800

    Create 404.cpp

---
+++ leetcode/404.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root, bool left = false) {
+        if (!root)
+            return 0;
+        
+        if (!root->left && !root->right && left)
+            return root->val;
+
+        return sumOfLeftLeaves(root->left, true) + sumOfLeftLeaves(root->right, false);
+    }
+};
