b462f886573482eeaac895f923e46401b14842dd
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-06-03 20:00:16 +0800

    Create 76.cpp

---
+++ leetcode/76.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+	string solve(const string &s, vector<int> &shist, const vector<int> &thist) {
+		string s1 = s;
+		if (shist[s.front()] > thist[s.front()]) {
+			shist[s.front()]--;
+			s1 = solve(s.substr(1, s.size() - 1), shist, thist);
+			shist[s.front()]++;
+		}
+		string s2 = s;
+		if (shist[s.back()] > thist[s.back()]) {
+			shist[s.back()]--;
+			s2 = solve(s.substr(0, s.size() - 1), shist, thist);
+			shist[s.back()]++;
+		}
+		return s1.size() < s2.size() ? s1 : s2;
+	}
+
+	string minWindow(string s, string t) {
+		vector<int> shist(256, 0), thist(256, 0);
+		for (int i = 0; i < s.size(); i++)
+			shist[s[i]]++;
+		for (int i = 0; i < t.size(); i++)
+			thist[t[i]]++;
+		for (int i = 0; i < 256; i++)
+			if (shist[i] < thist[i])
+				return "";
+		return solve(s, shist, thist);
+	}
+};
