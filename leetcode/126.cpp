33eb2ccfa3ae973c260e7c99c2ecb85507844416
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-08-10 23:55:25 +0800

    Update 126.cpp

--- leetcode/126.cpp
+++ leetcode/126.cpp
@@ -1,96 +1,122 @@
-// Can't memorize solution, because it's hard to encode 'visited' state. Naive DFS leads
-// to LTE.
-
-class Solution {
+class Solution
+{
 public:
-	int n;
-	vector<vector<bool>> adjacent;
-	typedef vector<int> Path;
-	typedef vector<Path> Paths;
-	vector<bool> visited;
+  int wordDiff(const string &lw, const string &rw) {
+    assert(lw.size() == rw.size());
 
-	bool is_adjacent(const string &a, const string &b) {
-		int cnt = 0;
-		for (int i = 0; i < a.size(); i++) {
-			if (a[i] != b[i] && ++cnt > 1)
-				return false;
-		}
-		return true;
-	}
+    int cnt = 0;
+    for (int i = 0; i < lw.size(); ++i) {
+      if (lw[i] != rw[i]) {
+        if (++cnt > 1)
+          return cnt;
+      }
+    }
+    return cnt;
+  }
 
-	pair<int, Paths> solve(int ci, int ei) {
-		if (ci == ei) {
-			Paths paths;
-			paths.push_back(Path(1, ci));
-			return make_pair(1, paths);
-		}
+  vector<vector<int> > table, from;
+  vector<int> dists;
+  int ei, n;
 
-		visited[ci] = true;
-		Paths paths;
-		int l = INT_MAX;
-		for (int i = 0; i < n; i++) {
-			if (visited[i] || !adjacent[ci][i])
-				continue;
+  vector<vector<string> > buildPath(int cur, const vector<string> &wl) {
+    vector<vector<string> > vvs;
 
-			pair<int, Paths> i2ei = solve(i, ei);
-			int curl = i2ei.first;
-			const Paths &curpaths = i2ei.second;
-			if (curl == INT_MAX)
-				continue;
-			if (curl < l) {
-				l = curl;
-				paths.clear();
-			}
-			if (curl <= l) {
-				for (int j = 0; j < curpaths.size(); j++) {
-					Path path;
-					path.push_back(ci);
-					path.insert(path.end(), curpaths[j].begin(), curpaths[j].end());
-					paths.push_back(path);
-				}
-			}
-		}
-		visited[ci] = false;
-		return make_pair(l == INT_MAX ? INT_MAX : l + 1, paths);
-	}
+    sort(from[cur].begin(), from[cur].end());
+    vector<int>::iterator it = unique(from[cur].begin(), from[cur].end());
 
-	vector<vector<string>> findLadders(const string &bw, const string &ew, vector<string> wl) {
-		int bi = -1, ei = -1;
-		for (int i = 0; i < wl.size(); i++) {
-			if (bw == wl[i])
-				bi = i;
-			if (ew == wl[i])
-				ei = i;
-		}
-		if (ei == -1)
-			return vector<vector<string>>();
-		if (bi == -1) {
-			wl.insert(wl.begin(), bw);
-			bi = 0;
-			ei++;
-		}
-		
-		n = wl.size();
-		adjacent = vector<vector<bool>>(n, vector<bool>(n, false));
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				if (is_adjacent(wl[i], wl[j]))
-					adjacent[i][j] = adjacent[j][i] = true;
-			}
-		}
-		visited = vector<bool>(n, false);
+    for (int i = 0; i < it - from[cur].begin(); i++) {
+      vector<vector<string> > _vvs = buildPath(from[cur][i], wl);
+      vvs.insert(vvs.end(), _vvs.begin(), _vvs.end());
+    }
+    if (vvs.size() > 0) {
+      for (int i = 0; i < vvs.size(); i++) {
+        vvs[i].insert(vvs[i].begin(), wl[cur]);
+      }
+    } else {
+      vvs.push_back( vector<string>(1, wl[cur]) );
+    }
+    return vvs;
+  }
 
-		vector<vector<string>> rv;
-		pair<int, Paths> solve_rv = solve(bi, ei);
-		const Paths &paths = solve_rv.second;
-		for (int i = 0; i < paths.size(); i++) {
-			const Path &path = paths[i];
-			vector<string> spath(1, bw);
-			for (int j = 1; j < path.size(); j++) {
-				spath.push_back(wl[path[j]]);
-			}
-			rv.push_back(spath);
-		}
-		return rv;
-	}
+  vector<vector<string> > buildPath(vector<int> starts, const string &bw, const vector<string> &wl) {
+    vector<vector<string> > vvs;
+
+    for (int i = 0; i < starts.size(); i++) {
+      vector<vector<string> > _vvs = buildPath(starts[i], wl);
+      for (int j = 0; j < _vvs.size(); j++) {
+        _vvs[j].insert(_vvs[j].begin(), bw);
+      }
+      vvs.insert(vvs.end(), _vvs.begin(), _vvs.end());
+    }
+    return vvs;
+  }
+
+  vector<vector<string> > findLadders(string bw, string ew, const vector<string> &wl) {
+    vector<vector<string> > vvs;
+    ei = -1;
+    n = wl.size();
+    table = vector<vector<int> >(n);
+    dists = vector<int>(n, INT_MAX);
+    from = vector<vector<int> >(n);
+
+    for (int i = 0; i < n; i++) {
+      if (wl[i] == ew)
+        ei = i;
+      for (int j = i + 1; j < n; j++) {
+        if (wordDiff(wl[i], wl[j]) == 1) {
+          table[i].push_back(j);
+          table[j].push_back(i);
+        }
+      }
+    }
+    if (ei < 0)
+      return vvs;
+
+    int pathlen = INT_MAX;
+    dists[ei] = 0;
+
+    vector<bool> match(n);
+    for (int i = 0; i < n; ++i) {
+      match[i] = wordDiff(bw, wl[i]) == 1;
+    }
+
+    queue<int> q;
+    q.push(ei);
+    while (!q.empty()) {
+      int nxt = q.front();
+      q.pop();
+
+      if (dists[nxt] > pathlen)
+        break;
+      if (match[nxt]) {
+        if (pathlen == INT_MAX) {
+          pathlen = dists[nxt];
+        }
+      }
+
+      for (vector<int>::iterator it = table[nxt].begin(); it != table[nxt].end(); it++) {
+        int i = *it;
+        int d = dists[nxt] + 1;
+        //printf("%s to %s: %d\n", wl[nxt].c_str(), wl[i].c_str(), d);
+        if (d <= dists[i]) {
+          if (d < dists[i]) {
+            from[i].clear();
+          }
+        //printf("%s to %s: %d\n", wl[nxt].c_str(), wl[i].c_str(), d);
+          dists[i] = d;
+          from[i].push_back(nxt);
+          q.push(i);
+        }
+      }
+    }
+
+    vector<int> starts;
+    for (int i = 0; i < n; ++i) {
+      if (match[i] && dists[i] == pathlen)
+        starts.push_back(i);
+    }
+    vvs = buildPath(starts, bw, wl);
+
+    return vvs;
+  }
 };
