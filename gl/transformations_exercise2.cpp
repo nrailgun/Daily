3e93027e9890c5589f51e1d86e318a07f08be304
Junyu Wu <shibuyanorailgun@foxmail.com>
2017-12-07 01:09:59 +0800

    Create transformations_exercise2.cpp

---
+++ gl/transformations_exercise2.cpp
@@ -0,0 +1,110 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "gl_warp.h"
+#include "Shader.h"
+#include "stb_image.h"
+
+const char *vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;"
+"layout (location = 1) in vec2 aTexCoord;"
+"out vec2 TexCoord;"
+"uniform mat4 transform;"
+"void main() {"
+"	gl_Position = transform * vec4(aPos, 1.0);"
+"	TexCoord = aTexCoord;"
+"}";
+
+const char *fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;"
+"in vec2 TexCoord;"
+"uniform sampler2D texture1;"
+"uniform sampler2D texture2;"
+"void main() {"
+"	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(TexCoord.x, TexCoord.y)), .2);"
+"}";
+
+void processInput(GLFWwindow *window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+	float vertices[] = {
+		0.5f, 0.5f, 0.0f, 1.0f, 1.0f, // top right
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+		-0.5f, 0.5f, 0.0f, 0.0f, 1.0f  // top left
+	};
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3,
+	};
+
+	GLFWwindow *window = gl_warp_init();
+	Shader ourShader(vertexShaderSource, fragmentShaderSource, true);
+
+	unsigned int vbo, vao, ebo;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ebo);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	unsigned int texture1 = texture_load("resources/textures/container.jpg");
+	unsigned int texture2 = texture_load("resources/textures/awesomeface.png", GL_RGBA);
+	ourShader.use();
+	ourShader.setInt("texture1", 0);
+	ourShader.setInt("texture2", 1);
+
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+		glClearColor(.2, .3, .3, 1.);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		glm::mat4 trans;
+		trans = glm::translate(trans, glm::vec3(.5, -.5, 0));
+		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0, 0, 1));
+		ourShader.setMat4("transform", trans);
+		ourShader.use();
+		glBindVertexArray(vao);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		trans = glm::mat4();
+		trans = glm::translate(trans, glm::vec3(-.5, .5, 0));
+		float timev = glfwGetTime();
+		timev = sin(timev);
+		trans = glm::scale(trans, glm::vec3(timev, timev, timev));
+		ourShader.setMat4("transform", trans);
+		ourShader.use();
+		glBindVertexArray(vao);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+	glDeleteBuffers(1, &ebo);
+	glfwTerminate();
+	return EXIT_SUCCESS;
+}
