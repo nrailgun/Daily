f341fadc292b6f2321a386a8178243e4137b7d3e
Junyu Ngo <shibuyanorailgun@foxmail.com>
2020-02-08 18:38:21 +0800

    Update 917.cpp

--- leetcode/917.cpp
+++ leetcode/917.cpp
@@ -7,7 +7,9 @@ public:
 		char *ocs = (char *) o.c_str();
 
 		while (i < j) {
-      // 下面的 case 避免 i<j 冗余的判断条件会显著提高效率。按理一个多余的 cmp 运算没什么所谓，可能是复杂的条件不利于编译器的优化。
+			// 下面的 case 避免 i<j 冗余的判断条件会显著提高效率。按理一个多余的 cmp 运算没什么所谓，
+			// 可能是复杂的条件不利于编译器的优化。试过 rustc 多一个条件并不会影响运行效率，我觉得这
+			// 可能是 leetcode 的 g++ 编译器的 bug。
 			if (!isalpha(ocs[i]))
 				i++;
 			else if (!isalpha(ocs[j]))
