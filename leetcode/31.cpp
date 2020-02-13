b7a6f9a9a83a4ad67e76455cdd6a9e4825613272
Junyu Ngo <shibuyanorailgun@foxmail.com>
2020-02-13 20:59:25 +0800

    Update 31.cpp

--- leetcode/31.cpp
+++ leetcode/31.cpp
@@ -20,3 +20,36 @@ public:
         reverse(p + j + 1, p + n);
     }
 };
+
+// 几年后又默写了一个版本，代码更难看了....
+class Solution {
+public:
+	bool nextPermutation(vector<int> &a) {
+		int n = a.size();
+		if (n < 2) {
+			return false;
+		}
+
+		int nxtI = n - 2;
+		while (nxtI >= 0 && a[nxtI] >= a[nxtI + 1]) {
+			nxtI--;
+		}
+		if (nxtI < 0) {
+			reverse(a.begin(), a.end());
+			return false;
+		}
+
+		int swpI = n - 1;
+		while (true) {
+			if (a[swpI] > a[nxtI]) {
+				swap(a[swpI], a[nxtI]);
+				break;
+			}
+			swpI--;
+		}
+		vector<int>::iterator it = a.begin();
+		advance(it, nxtI + 1);
+		reverse(it, a.end());
+		return true;
+	}
+};
