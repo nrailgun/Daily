08fd190e36a4831a734756e6d691bb76de76ed24
Junyu Wu <shibuyanorailgun@foxmail.com>
2020-04-14 23:37:43 +0800

    Update 151.cpp

--- leetcode/151.cpp
+++ leetcode/151.cpp
@@ -2,66 +2,30 @@
 
 class Solution {
 public:
-    void reverseWords(string &s) {
-        string output(s);
-        int n = s.size();
-        int last = 0;
-        
-        int i = n - 1;
-        while (i >= 0 && s[i] == ' ')
-            i--;
-        while (1) {
-            if (i >= 0 && s[i] != ' ') {
-                i--;
-            } else {
-                for (int j = 1; i + j < n && s[i + j] != ' '; j++)
-                    output[last++] = s[i + j];
-                while (i >= 0 && s[i] == ' ')
-                    i--;
-                if (i < 0)
-                    break;
-                else
-                    output[last++] = ' ';
-            }
-        }
-        output.resize(last);
-        s = output;
-    }
+	string reverseWords(string s) {
+		int n = s.size();
+		int i = 0, j;
+		for (j = 0; j < n; ) {
+			if (s[j] != ' ')
+				s[i++] = s[j++];
+			else {
+				while (j < n && s[j] == ' ')
+					j++;
+				if (i != 0 && j < n)
+					s[i++] = ' ';
+			}
+		}
+		n = i;
+		s.resize(n);
+		reverse(s.begin(), s.end());
+
+		for (i = 0; i < n; ) {
+			j = 1;
+			while (i + j < n && s[i + j] != ' ')
+				j++;
+			reverse(s.begin() + i, s.begin() + i + j);
+			i += ++j;
+		}
+		return s;
+	}
 };
-
-// O(1) 空间，O(n) 时间。算法很简单，一次性正确实现却非常困难。
-
-void reverse(char *s, int n) {
-    int i, tmp;
-    for (i = 0; i < n / 2; i++) {
-        tmp = s[i];
-        s[i] = s[n - 1 - i];
-        s[n - 1 - i] = tmp;
-    }
-}
-
-void reverseWords(char *s) {
-    int n = strlen(s);
-    int i = 0, j;
-    for (j = 0; j < n; ) {
-        if (s[j] != ' ')
-            s[i++] = s[j++];
-        else {
-            while (j < n && s[j] == ' ')
-                j++;
-            if (i != 0 && j < n)
-                s[i++] = ' ';
-        }
-    }
-    s[i] = 0;
-    n = i;
-    reverse(s, n);
-    
-    for (i = 0; i < n; ) {
-        j = 1;
-        while (i + j < n && s[i + j] != ' ')
-            j++;
-        reverse(s + i, j);
-        i += ++j;
-    }
-}
