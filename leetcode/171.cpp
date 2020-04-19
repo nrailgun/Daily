c7414ed96b4c2fc55cc0da5eed3a1d25822b3ef1
Junyu Wu <shibuyanorailgun@foxmail.com>
2020-04-19 19:50:19 +0800

    Update 171.cpp

--- leetcode/171.cpp
+++ leetcode/171.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int titleToNumber(const string &s) {
-        int v = 0;
-        int base = 1;
+        int64_t v = 0;
+        int64_t base = 1;
         
         for (int i = int(s.size()) - 1; i >= 0; i--) {
             v += base * (s[i] - 'A' + 1);
