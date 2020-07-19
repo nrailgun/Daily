ca5997074b29db28f308d80dfe1ab7764fdf2f06
Junyu Wu <junyuwu1992@gmail.com>
2020-07-19 21:28:52 +0800

    Update 207.cpp

--- leetcode/207.cpp
+++ leetcode/207.cpp
@@ -2,33 +2,38 @@
 
 class Solution {
 public:
-    bool canFinish(int n, vector<pair<int, int>>& pres) {
-        vector<int> inds(n, 0);
-        vector<vector<int>> prem(n, vector<int>());
-        
-        for (auto pr : pres) {
-            inds[pr.second]++;
-            prem[pr.first].push_back(pr.second);
-        }
-        
-        queue<int> zins;
-        for (int i = 0; i < n; i++) {
-            if (inds[i] == 0)
-                zins.push(i);
-        }
-        
-        int ok = 0;
-        while (!zins.empty()) {
-            int zin = zins.front();
-            zins.pop();
-            ok++;
-            
-            for (int j : prem[zin]) {
-                if (0 == --inds[j]) {
-                    zins.push(j);
-                }
-            }
-        }
-        return ok == n;
-    }
+	bool canFinish(int n, vector<vector<int>> &pres) {
+		if (n == 0) {
+			return true;
+		}
+
+		vector<vector<int>> premat(n, vector<int>());
+		vector<int> inds(n, 0);
+		for (const auto &pre : pres) {
+			int a = pre[0];
+			int b = pre[1]; // b goes before a
+			inds[a]++;
+			premat[b].push_back(a);
+		}
+
+		queue<int> zins;
+		for (int i = 0; i < n; i++) {
+			if (inds[i] == 0) {
+				zins.push(i);
+			}
+		}
+		int ok = 0;
+		while (!zins.empty()) {
+			int a = zins.front();
+			zins.pop();
+			ok++;
+
+			for (int b : premat[a]) {
+				if (--inds[b] == 0) {
+					zins.push(b);
+				}
+			}
+		}
+		return ok == n;
+	}
 };
